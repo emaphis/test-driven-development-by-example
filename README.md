4e806fbc6def458a704c904e0bb4cf929814bcb1
Ed Maphis <emaphis85@gmail.com>
2021-02-05 09:23:33 -0500

    Add money project.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # test-driven-development-by-example
 Test Driven Development by Example by Kent Beck
+
+money - money example from "Test Driven Development by Example"
---
+++ money/src/main/java/com/testdriven/money/Main.java
@@ -0,0 +1,13 @@
+
+package com.testdriven.money;
+
+/**
+ * Money App
+ * Test drives the money classes
+ * @author emaphis
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Money Aoo.");
+    }
+}
