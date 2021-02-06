4e0f21da4241729b08ba1bf8fde375f792ebe422
Ed Maphis <emaphis85@gmail.com>
2021-02-06 16:27:21 -0500

    Created factory methods for Money class to return Dollar of Francs to make code more general. Update test code to use factory methods. Generalized multiply methods to be abstract and to return the base class.

--- money/src/main/java/com/testdriven/money/Money.java
+++ money/src/main/java/com/testdriven/money/Money.java
@@ -5,8 +5,18 @@ package com.testdriven.money;
  * Common base class for Currency classes
  * @author emaphis
  */
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    public abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object obj) {
