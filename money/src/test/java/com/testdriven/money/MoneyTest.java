999179834b85d56b41d72efa4fb0c9535ce161ac
Ed Maphis <emaphis85@gmail.com>
2021-02-06 18:09:52 -0500

    Eliminate unneeded Dollar and Franc classes.

--- money/src/test/java/com/testdriven/money/MoneyTest.java
+++ money/src/test/java/com/testdriven/money/MoneyTest.java
@@ -10,27 +10,13 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class MoneyTest {
 
-    @Test
-    public void testDollarMultiplication() {
-        Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10), five.times(2));
-        assertEquals(Money.dollar(15), five.times(3));
-    }
 
     @Test
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
         assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
-    }
-
-    @Test
-    public void testFrankMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
@@ -46,8 +32,4 @@ public class MoneyTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-    }
 }
