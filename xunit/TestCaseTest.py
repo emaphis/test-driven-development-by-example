23af73c1386ad56b55abf0ab6dc877368b09c044
emaphis <emaphis85@gmail.com>
2022-02-02 17:30:49 -0500

    One more clean-up

--- xunit/TestCaseTest.py
+++ xunit/TestCaseTest.py
@@ -1,15 +1,13 @@
-from unittest import TestResult
 from TestCase import TestCase
 from WasRun import WasRun
 from TestResult import TestResult
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
 
     def testTemplateMethod(self):
-        self.test.run()
-        assert("setUp testMethod tearDown " == self.test.log)
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
     def testResult(self):
         test = WasRun("testMethod")
